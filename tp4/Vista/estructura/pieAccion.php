0b51d7abdf2cb33cd8d67159ffd43a180945f0f1
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-10-06 22:22:42 -0300

    añadido enlace a raíz del proyecto en footer de las páginas

--- tp4/Vista/estructura/pieAccion.php
+++ tp4/Vista/estructura/pieAccion.php
@@ -6,6 +6,9 @@
         <h5 class="text-light">Programación Web Dinámica</h5>
         <strong class="text-light">Grupo 2: Peralta-Romero-Gaspart</strong>
     </div>
+    <div class="container text-center">
+            <a href="../../../index.html" style="color: #70fd9c;">Raiz del proyecto</a>
+        </div>
 </footer>
 <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 <script src="../js/validacion.js"></script>
