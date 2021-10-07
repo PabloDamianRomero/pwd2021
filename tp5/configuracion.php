7aea2325cc0711ef7c253a76a65c42b2a4147ca5
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-10-07 14:58:54 -0300

    cambiada la ruta del proyecto en configuracion

--- tp5/configuracion.php
+++ tp5/configuracion.php
@@ -5,7 +5,7 @@ header ("Cache-Control: no-cache, must-revalidate ");
 // CONFIGURACION APP//
 /////////////////////////////
 
-$PROYECTO ='tp5';
+$PROYECTO ='pwd2021/tp5';
 
 //variable que almacena el directorio del proyecto
 $ROOT =$_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
